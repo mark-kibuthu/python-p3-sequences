4a02603a09e336c130c0d03df450ab0deebcb1eb
Agent-grey <markkibuthu@gmail.com>
2024-09-04 17:21:13 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0, 1]
+
+    for i in range(2, length):
+        next_value = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_value)
+
+    print(fibonacci_list[:length])
+
+print_fibonacci(9)
